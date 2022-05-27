d6ecd2fcb279726fc2bc9503807970778dfa6724
nidhi.r.patel <nidhi.r.patel@cgi.com>
2022-05-27 11:55:35 -0400

    Comparable updates

--- Collection_Framework/collection/ComparableInterface/src/App.java
+++ Collection_Framework/collection/ComparableInterface/src/App.java
@@ -53,12 +53,24 @@ public class App {
 //		Collections.sort(countries);
 //		new App().displayList(countries);
 		
-		List name = new LinkedList<>();
+		List<Names> name = new LinkedList<>();
 		name.add(new Names("Nidhi"));
 		name.add(new Names("Sunny"));
 		name.add(new Names("Jeni"));
 		name.add(new Names("Janvi"));
 		name.add(new Names("Manya"));
+		
+		
+		//using List<Object> if we need not want to restrict to only Names type
+		List<Object> elements = new LinkedList<>();
+		elements.add(new Names("Nidhi"));
+		elements.add(new Names("Shaheera"));
+		elements.add(new Names("Palak"));
+		elements.add("Some String");
+		elements.add(1);
+		elements.add("$");
+		
+		
 		
 		App app = new App();
 		app.displayList(name);
@@ -66,6 +78,9 @@ public class App {
 		Collections.sort(name);
 		System.out.println("*************");
 		app.displayList(name);
+		
+		System.out.println("******************");
+		app.printList(elements);
 	}
 	
 	void displayList(List<Names> list) {
@@ -73,6 +88,12 @@ public class App {
 		while(iterator.hasNext()) {
 			System.out.println("Element: "+iterator.next());
 		}
+	}
+		void printList(List<Object> list) {
+			ListIterator<Object> iterator = list.listIterator();
+			while(iterator.hasNext()) {
+				System.out.println("Element: "+iterator.next());
+			}
 	}
 
 }
